10a8903548b772148a9538be0d22e0651bf62ef2
mah1ru0 <masevah@gmail.com>
2020-12-02 18:41:12 -0800

    Completed Exercises

---
+++ ChapterExs/Ch6 functions/Ch6 Exercises/5x2_SpiralSquare.py
@@ -0,0 +1,25 @@
+import turtle
+
+x = 500
+ang = 91
+travle = 5
+
+def spiralSquare(t, dis):
+        t.rt(ang)
+        t.fd(dis)
+
+def main():
+    wn = turtle.Screen()
+    wn.bgcolor('black')
+    burt = turtle.Turtle()
+    burt.color('lightgreen')
+    burt.pensize(2)
+    burt.speed(10)
+    travle = 3
+    for i in range(x):
+        spiralSquare(burt, travle)
+        travle = travle + 3
+    wn.exitonclick()
+
+main()
+
